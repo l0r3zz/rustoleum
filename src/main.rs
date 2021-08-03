41c835d6a1d469575dfdeb60021b123dd43d281e
Geoff White <geoffw@durganetworks.com>
2021-08-03 09:28:13 -0700

    refactored command line input

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
 use std::{error, result};
-use std::env;
 use std::collections::HashMap;
+use structopt::StructOpt;
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rustoleum")]
+
+struct Opt {
+    /// this argument holds the unit of measurement of the input value
+    #[structopt(short = "i", long = "uom_in", default_value = "fahrenheit")]
+    uom_in: String,
+    /// this argument holds the unit of measurement of the target value
+    #[structopt(short = "t", long = "uom_target", default_value = "celsius")]
+    uom_target: String,
+    /// The proctor control input
+    control: String,
+    /// The student answer
+    answer: String,
+}
 
 //#[derive(debug)]
 //    for arg in env::args()
 
 // macro to make hasmap initialization easy
-macro_rules! hashmap {
-    ($( $key: expr => $val: expr ),*) => {{
-         let mut map = HashMap::new();
-         $( map.insert($key, $val); )*
-         map
-    }}
-}
+//macro_rules! hashmap {
+//    ($( $key: expr => $val: expr ),*) => {{
+//         let mut map = HashMap::new();
+//         $( map.insert($key, $val); )*
+//         map
+//    }}
+//}
 
 
-type TResult<T> = result::Result<T, TError>;
-type TError = Box<dyn error::Error>;
-
-//fn get_args() -> TResult<HashMap>{
-//fn get_args(cmds:&mut HashMap<&str,i32>) -> TResult<HashMap<&str,i32>>{
-fn get_args<'a>(cmds:&'a mut HashMap<&str,i32>) -> TResult<HashMap<&'a str,i32>>{
-    unimplemented!()
-}
+//type TResult<T> = result::Result<T, TError>;
+//type TError = Box<dyn error::Error>;
 
 
 fn main() {
-//let hash_result = hashmap!['A' => 0, 'C' => 0, 'G' => 0, 'T' => 0];
+    let opts = Opt::from_args();
+    let uom_in = opts.uom_in;
+    let uom_target = opts.uom_target;
+    let control = opts.control;
+    let answer = opts.answer;
+    println!("Value for uom_in: {}", uom_in.to_ascii_uppercase());
+    println!("Value for uom_target: {}", uom_target.to_ascii_uppercase());
+    println!("Value for control: {}", control);
+    println!("Value for answer: {}", answer);
     println!("Hello, world!");
 }
 
@@ -38,13 +55,5 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-    }
-    #[test]
-    fn test_get_args() {
-        use crate::get_args;
-        use std::collections::HashMap;
-        let mut hash_result = hashmap!["A" => 0, "C" => 0, "G" => 0, "T" => 0];
-        let res = get_args(&mut hash_result);
-        assert!(res.is_ok());
     }
 }

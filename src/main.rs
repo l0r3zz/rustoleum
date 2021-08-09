23d5ac4ac904e6139e69b6e4f3c9e1dd11cee551
Geoff White <geoffw@durganetworks.com>
2021-08-08 20:34:34 -0700

    Added lib dir and -v switch

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use structopt::StructOpt;
+use rustoleum::*;
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = "rustoleum", author="geoffw")]
 
@@ -8,6 +10,8 @@ struct Opt {
     /// this argument holds the unit of measurement of the input value
     #[structopt(short = "i", long = "uom_in", default_value = "fahrenheit")]
     uom_in: String,
+    #[structopt(short = "v", long = "verbose")]
+    verbose: bool,
     /// this argument holds the unit of measurement of the target value
     #[structopt(short = "t", long = "uom_target", default_value = "celsius")]
     uom_target: String,
@@ -16,8 +20,6 @@ struct Opt {
     /// The student answer
     answer: String,
 }
-
-//#[derive(debug)]
 
 // macro to make hasmap initialization easy
 macro_rules! hashmap {
@@ -28,199 +30,8 @@ macro_rules! hashmap {
     }}
 }
 
-// Function to asertain approximate equality of f64 numbers up to specified
-// decimal places
-fn approx_eq(a: f64, b: f64, decimal_places: u8) -> bool {
-    let factor = 10.0f64.powi(decimal_places as i32);
-    let a = (a * factor).trunc();
-    let b = (b * factor).trunc();
-    a == b
-}
-
  // function pointer type
  type Measureop = fn(f64) -> f64;
-
-// List of conversion functions for all supported metrics
-
-// Temperature conversions
-// ----------------------------------------
-// kelvin to celsius conversion function
-fn kel_cel(n:f64) -> f64 {
-    n - 273.15
-}
-// kelvin to fahrenheit conversion function
-fn kel_fah(n:f64) -> f64 {
-    (n - 273.15) * 9.0/5.0 + 32.0
-}
-//kelvin to rankine conversion function
-fn kel_ran(n:f64) -> f64 {
-    n * 1.8
-}
-//celsius to kelvin conversion function
-fn cel_kel(n:f64) -> f64 {
-    n + 273.15
-}
-//celsius to fahrenheit conversion function
-fn cel_fah(n:f64) -> f64 {
-    (n * 9.0/5.0) + 32.0
-}
-//celsius to rankine conversion function
-fn cel_ran(n:f64) -> f64 {
-    (n * 9.0/5.0) + 491.67
-}
-//fahrenheit to kelvin conversion function
-fn fah_kel(n:f64) -> f64 {
-    (n - 32.0)* 5.0/9.0 + 273.15
-}
-//fahrenheit to celsius conversion function
-fn fah_cel(n:f64) -> f64 {
-    (n - 32.0)* 5.0/9.0
-}
-//fahrenheit to rankine conversion function
-fn fah_ran(n:f64) -> f64 {
-    n  + 459.67
-}
-//rankine to kelvin conversion function
-fn ran_kel(n:f64) -> f64 {
-    (n - 32.0)* 9.0/5.0 + 273.15
-}
-//rankine to celsius conversion function
-fn ran_cel(n:f64) -> f64 {
-    (n - 32.0)* 9.0/5.0
-}
-//rankine to fahrenheit conversion function
-fn ran_fah(n:f64) -> f64 {
-    n  + 459.67
-}
-
-// Volume conversions
-// ----------------------------------------
-// liters to tablespoon conversion function
-fn lit_tab(n:f64) -> f64 {
-    n  * 67.628
-}
-// liters to cubic-inches conversion function
-fn lit_ci(n:f64) -> f64 {
-    n  * 61.023
-}
-// liters to cups conversion function
-fn lit_cups(n:f64) -> f64 {
-    n  * 4.226
-}
-// liters to cubic-feet conversion function
-fn lit_cf(n:f64) -> f64 {
-   n  * 0.0353
-}
-// liters to gallons conversion function
-fn lit_gal(n:f64) -> f64 {
-   n  * 0.2641
-}
-
-// tablespoons to liters conversion function
-fn tab_lit(n:f64) -> f64 {
-    n  * 0.0147
-}
-// tablespoons to cubic-inches conversion function
-fn tab_ci(n:f64) -> f64 {
-    n  * 0.902
-}
-// tablespoons to cups conversion function
-fn tab_cups(n:f64) -> f64 {
-    n  * 0.062
-}
-// tablespoons to cubic-feet conversion function
-fn tab_cf(n:f64) -> f64 {
-   n  * 0.00052219
-}
-// tablespoons to gallons conversion function
-fn tab_gal(n:f64) -> f64 {
-   n  * 0.00390625
-}
-// cubic-inches to liters conversion function
-fn ci_lit(n:f64) -> f64 {
-   n  * 0.0163
-}
-// cubic-inches to 0.000578704tablespoons conversion function
-fn ci_tab(n:f64) -> f64 {
-   n  * 1.10823
-}
-// cubic-inches to cups conversion function
-fn ci_cups(n:f64) -> f64 {
-   n  * 0.06926
-}
-// cubic-inches to cubic-feet conversion function
-fn ci_cf(n:f64) -> f64 {
-   n  * 0.000578704
-}
-// cubic-inches to gallons conversion function
-fn ci_gal(n:f64) -> f64 {
-   n  * 0.004329
-}
-
-
-// cups to liters conversion function
-fn cups_lit(n:f64) -> f64 {
-   n  * 0.236588
-}
-
-// cups to cubic-inches conversion function
-fn cups_ci(n:f64) -> f64 {
-   n  * 14.4375
-}
-// cups to tablespoons conversion function
-fn cups_tab(n:f64) -> f64 {
-   n  * 16.0
-}
-// cups to cubic-feet conversion function
-fn cups_cf(n:f64) -> f64 {
-   n  * 0.00835
-}
-// cups to gallons conversion function
-fn cups_gal(n:f64) -> f64 {
-   n  * 0.0625
-}
-// cubic-feet to liters conversion function
-fn cf_lit(n:f64) -> f64 {
-   n  * 28.3168
-}
-// cubic-feet to cubic-inches conversion function
-fn cf_ci(n:f64) -> f64 {
-   n  * 1728.0
-}
-// cubic-feet to tablespoons conversion function
-fn cf_tab(n:f64) -> f64 {
-   n  * 1915.01
-}
-// cubic-feet to cups conversion function
-fn cf_cups(n:f64) -> f64 {
-   n  * 119.688
-}
-// cubic-feet to gallons conversion function
-fn cf_gal(n:f64) -> f64 {
-   n  * 7.48052
-}
-// gallons to liters conversion function
-fn gal_lit(n:f64) -> f64 {
-   n  * 3.785
-}
-// gallons to cubic-inches conversion function
-fn gal_ci(n:f64) -> f64 {
-   n  * 231.0
-}
-// gallons to tablespoons conversion function
-fn gal_tab(n:f64) -> f64 {
-   n  * 256.0
-}
-// gallons to cubic-feet conversion function
-fn gal_cf(n:f64) -> f64 {
-   n  * 0.133
-}
-// gallons to cups conversion function
-fn gal_cups(n:f64) -> f64 {
-   n  * 0.0625
-}
-
-
 
 fn main() {
     let opts = Opt::from_args();
@@ -408,10 +219,12 @@ fn main() {
         "GALLONS" => gallons_map
     ];
 
-    println!("Value for uom_in: {}", args_ctx["uom_in"]);
-    println!("Value for uom_target: {}",args_ctx["uom_target"]);
-    println!("Value for control: {}", args_ctx["control"]);
-    println!("Value for answer: {}", args_ctx["answer"]);
+    if opts.verbose {
+	    println!("Value for uom_in: {}", args_ctx["uom_in"]);
+	    println!("Value for uom_target: {}",args_ctx["uom_target"]);
+	    println!("Value for control: {}", args_ctx["control"]);
+	    println!("Value for answer: {}", args_ctx["answer"]);
+    }
     match cvnmap.get(&*args_ctx["uom_in"]){
         Some(value) => {
             match value.get(&*args_ctx["uom_target"]){
@@ -438,54 +251,5 @@ fn main() {
         None => {
             println!("Answer: {}","invalid");
         }
-    }
-}
-
-
-
-// Unit test go here
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-    #[test]
-    fn test_output_result(){
-        assert!(true);
-    }
-    #[test]
-    // test Fahrenheit to Celsius functions
-    fn test_fah_cel() {
-        let f = 70.0;
-        let c = 21.11;
-        let f2c: Measureop = fah_cel;
-        let res_fn = fah_cel(f);
-        let res_fnp = f2c(f);
-        assert!(approx_eq(res_fn, c,2));
-        assert!(approx_eq(res_fnp, c,2));
-    }
-    #[test]
-    // test Fahrenheit to kelvin functions
-    fn test_fah_kel() {
-        let f = 70.0;
-        let k = 294.26;
-        let f2k: Measureop = fah_kel;
-        let res_fn = fah_kel(f);
-        let res_fnp = f2k(f);
-        assert!(approx_eq(res_fn, k,2));
-        assert!(approx_eq(res_fnp, k,2));
-    }
-    #[test]
-    // test Fahrenheit to Rankine functions
-    fn test_fah_ran() {
-        let f = 70.0;
-        let r = 529.67;
-        let f2r: Measureop = fah_ran;
-        let res_fn = fah_ran(f);
-        let res_fnp = f2r(f);
-        assert!(approx_eq(res_fn, r,3));
-        assert!(approx_eq(res_fnp, r,3));
     }
 }
